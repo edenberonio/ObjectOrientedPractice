7b7c1f3f5887fd3b82b53b197e7593264cc6d69d
Eden Beronio <edenberonio@gmail.com>
2017-05-23 12:47:27 -0400

    Add bank account exercise

---
+++ bankaccount.rb
@@ -0,0 +1,40 @@
+class Account
+    attr_reader:name
+    attr_reader:balance
+
+
+    def initialize(name,balance = 100)
+        @name= name
+        @balance= balance
+    end
+
+    private
+    def pin
+        @pin== 1234.num
+    end
+
+    private
+    def pin_error
+        " Access denied: incorrect PIN."
+    end
+
+    public
+    def display_balance(pin_number)
+        if pin_number == pin
+            puts "Balance: $#{balance}."
+        else
+            puts pin_error
+        end
+    end
+
+    public
+    def withdraw(pin_number, amount)
+        if pin_number == pin
+            puts "Withdrew #{amount}. New balance:       $#{@balance}."
+        else
+            puts pin_error
+        end
+    end
+end
+
+checking_account= Account.new("Eden",100)
